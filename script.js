0eba67892154bc687a56024fe6bd6604134890b3
richhuwae <122433871+richhuwae@users.noreply.github.com>
2023-01-11 14:57:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// select the button
+var button = document.querySelector('button');
+
+// add a click event listener to the button
+button.addEventListener('click', function() {
+    // change the text of the button when clicked
+    button.textContent = "Clicked!";
+});
 
